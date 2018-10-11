fd3c70b12388bee8eb99fa40acc62d4ba70dcdae
mp3616 <mp3616@skbl-422-004.ph.ic.ac.uk>
2018-10-11 15:27:40 +0100

    added 8 and 16 bit counters not tested

--- Simple1.asm
+++ Simple1.asm
@@ -8,14 +8,32 @@
 
 start
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movwf	0x06, ACCESS	    ; reset 0x06
 	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+loop	movlw	0xFF		    ; delay timer value
+	movwf	0x20, ACCESS	    ; sets counter lower bit
+	movwf	0x21, ACCESS	    ; sets uppper bit
+	call	delay16
+	movff 	0x06, PORTC
+	incf 	0x06, F, ACCESS
+test	movlw 	0x63		    ; test for end of loop condition
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 	
+; 8-bit delay subroutine
+delay8	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if result is zero
+	bra	delay8		    ; loop
+	return	0		    ; return to call
+	
+delay16	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if zero
+	bra	delay16		    ; loop
+	movlw	0xFF		    ; set 0x20 to 0xFF
+	movwf	0x20, ACCESS
+	decfsz	0x21, F, ACCESS	    ; decrement upper byte
+	bra	delay16		    ; loop
+	return	0		    ; return
+	
 	end
+	
