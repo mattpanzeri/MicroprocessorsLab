d244fa1efdefb59b730eec0acbd90ec7a6028270
mp3616 <mp3616@skbl-422-004.ph.ic.ac.uk>
2018-10-12 09:54:02 +0100

    added FSR0 write added FSR1 read and error check working

--- Simple1.asm
+++ Simple1.asm
@@ -6,16 +6,27 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+start	movlw	0x00
+	movwf	0x20, ACCESS	    ; set counter at 0x20
+	movwf	TRISC, ACCESS	    ; set PORTC to output
+	lfsr	FSR0, 0x300	    ; start from bank3 at 0x00
+	lfsr	FSR1, 0x3FE	    ; second FSR
+	movff	0x20, POSTINC0	    ; write 1st value
+wloop	incf	0x20, F, ACCESS	    ; increment counter
+	movff	0x20, POSTINC0	    ; write counter to FSR, with postincrement
+	movlw	0xFF		    ; compare counter to 0xFF
+	cpfseq	0x20, ACCESS	    ; if equal, skip ahead, if not, loop
+	bra	wloop
 	
+	movlw	0x32		    ; modify register 0x367 to see if program exits
+	movlb	0x03
+	movwf	0x67, BANKED
+	
+rloop	decf	0x20, F, ACCESS	    ; decrement counter
+	movf	POSTDEC1, W, ACCESS ; load FSR1 into W
+	cpfseq	0x20, ACCESS	    ; compare counter to FSR1
+	bra	err		    ; exit and write which address is wrong
+	bra     rloop		    ; loop
+err	movff	0x20, PORTC
 	end
+
