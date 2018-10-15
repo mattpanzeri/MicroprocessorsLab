1e23c06390514266f4db43dfe183bd7568f555f2
mp3616 <mp3616@skbl-422-004.ph.ic.ac.uk>
2018-10-15 16:38:56 +0100

    tested and working with 16 bit memory

--- Simple1.asm
+++ Simple1.asm
@@ -2,13 +2,13 @@
 	
 	code
 	org 0x0
-	goto	init
+	goto	start
 	org 0x100		    ; Main code starts here at address 0x100
 	
 	; address 0x10, bit 0 is the memory read/write address
 	; 0 is register 1 (CP1, OE1), 1 is register 2 (CP2, OE2)
 	
-init	;setup port D (control bus)
+start	;setup port D (control bus)
 	movlw	0x00
 	movwf	TRISD, ACCESS	    ; Set PORTD to output
 	movlw	0xFF
@@ -20,43 +20,52 @@ init	;setup port D (control bus)
 	setf	TRISE		    ; Tri-state PORTE
 	;setup ouput ports F-G for visualizing  data
 	movlw	0x00
-	movwf	TRISF, ACCESS	    ; set PORTF to ouput
-	movwf	TRISG, ACCESS	    ; set PORTG to ouput
-main	;write a byte to reg 1
-	movlw	0xA7
+	movwf	TRISH, ACCESS	    ; set PORTF to ouput
+	movwf	TRISJ, ACCESS	    ; set PORTG to ouput
+dostuff	;write a byte to reg 1 and read into port H
+	movlw	0xF9
 	movwf	0x20, ACCESS	    ; want to write 0xA7
 	bcf	0x10, 0, ACCESS	    ; set bit to 0, so write to 1st register
 	call	write		    ; write byte
 	call	read		    ; read from same register
-	movff	0x21, PORTF	    ; display contents of register in PORTF
-	GOTO	init		    ; loop back to start
+	movff	0x21, PORTH	    ; display contents of register in PORTF
+	;write a byte to reg 2 and read into port J
+	movlw	0xE5
+	movwf	0x20, ACCESS	    ; want to write 0xA7
+	bsf	0x10, 0, ACCESS	    ; set bit to 1, so write to 2nd register
+	call	write		    ; write byte
+	call	read		    ; read from same register
+	movff	0x21, PORTJ	    ; display contents of register in PORTJ
+	GOTO	start		    ; loop back to start
 	
 	; writes data 0x20 to memory
 write   movlw	0x00
 	movwf	TRISE, ACCESS	    ; set PORTE to output
-	movff	0x20, TRISE	    ; write data to bus
+	movff	0x20, PORTE	    ; write data to bus
 	btfss	0x10, 0, ACCESS	    ; check 0-bit of 0x10, skip if 1
-	movlw	0b11111110	    ; set CP1 to 0
+	movlw	0xFE		    ; set CP1 to 0
 	btfsc	0x10, 0 , ACCESS    ; check 0-bit of 0x10, skip if 0
-	movlw	0b11111011	    ; set CP2 to 0
+	movlw	0xFB		    ; set CP2 to 0
 	movwf	PORTD, ACCESS	    ; write to port D
 	NOP			    ; wait
 	movlw	0xFF
 	movwf	PORTD, ACCESS	    ; reset control bus to high
 	setf	TRISE		    ; Tri-state PORTE
+	return	0
 	
 	; read data from memory into 0x21
 read	movlw	0xFF
 	movwf	TRISE, ACCESS	    ; set PORTE to input
 	btfss	0x10, 0, ACCESS	    ; check 0-bit of 0x10, skip if 1
-	movlw	0b11111101	    ; set OE1 to 0
+	movlw	0xFD		    ; set OE1 to 0
 	btfsc	0x10, 0 , ACCESS    ; check 0-bit of 0x10, skip if 0
-	movlw	0b11110111	    ; set OE2 to 0
+	movlw	0xF7		    ; set OE2 to 0
 	movwf	PORTD, ACCESS	    ; write to port D
 	NOP			    ; wait
 	movff	PORTE, 0x21	    ; save to register 0x21
 	movlw	0xFF
 	movwf	PORTD, ACCESS	    ; reset control bus to HIGH
 	setf	TRISE		    ; Tri-state PORTE
+	return	0
 	
 	end
