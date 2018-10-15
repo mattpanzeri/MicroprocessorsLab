50c7cb54990bfc360f02c310d52d11001974613e
mp3616 <mp3616@skbl-422-004.ph.ic.ac.uk>
2018-10-15 12:00:25 +0100

    added 24-bit delay

--- Simple1.asm
+++ Simple1.asm
@@ -9,24 +9,25 @@
 start
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movwf	TRISJ, ACCESS
 	movwf	0x06, ACCESS	    ; reset 0x06
 	bra 	test
 loop	movlw	0xFF		    ; delay timer value
+	movwf	0x21, ACCESS
 	movwf	0x20, ACCESS	    ; sets counter lower bit
-	movwf	0x21, ACCESS	    ; sets uppper bit
-	call	delay16
-	movwf	0x20, ACCESS	    ; sets counter lower bit
-	movwf	0x21, ACCESS	    ; sets uppper bit
-	call	delay16
-	movwf	0x20, ACCESS	    ; sets counter lower bit
-	movwf	0x21, ACCESS	    ; sets uppper bit
+	btg	PORTJ, 1, ACCESS    
 	call	delay16
 	movff 	0x06, PORTC
+	movlw	0x02
+	movwf	0x20, ACCESS
+	call	delay8
 	incf 	0x06, F, ACCESS
 test	movlw 	0xFE		    ; test for end of loop condition
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+	
+	
 	
 ; 8-bit delay subroutine
 delay8	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if result is zero
@@ -35,10 +36,23 @@ delay8	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if result is zero
 	
 delay16	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if zero
 	bra	delay16		    ; loop
-	movlw	0xFF		    ; set 0x20 to 0xFF
+	movlw	0x0F		    ; set 0x20 to 0xFF
 	movwf	0x20, ACCESS
 	decfsz	0x21, F, ACCESS	    ; decrement upper byte
 	bra	delay16		    ; loop
+	return	0		    ; return
+	
+delay24	decfsz	0x20, F, ACCESS	    ; decrement reg 0x20, skip if zero
+	bra	delay24		    ; loop
+	movlw	0xFF		    ; set 0x20 to 0xFF
+	movwf	0x20, ACCESS
+	decfsz	0x21, F, ACCESS	    ; decrement upper byte
+	bra	delay24		    ; loop
+	movlw	0xFF
+	movwf	0x20, ACCESS
+	movwf	0x21, ACCESS
+	decfsz	0x22, F, ACCESS
+	bra	delay24
 	return	0		    ; return
 	
 	end
