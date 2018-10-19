1494dfa49b442996182dd7d6ca07b5e8533d4ab5
mp3616 <mp3616@skbl-422-004.ph.ic.ac.uk>
2018-10-19 09:14:00 +0100

    bhdfgkhslkj

--- Simple1.asm
+++ Simple1.asm
@@ -5,71 +5,26 @@
 	goto	start
 	org 0x100		    ; Main code starts here at address 0x100
 	
-	; address 0x10, bit 0 is the memory read/write address
-	; 0 is register 1 (CP1, OE1), 1 is register 2 (CP2, OE2)
 	
-start	;setup port D (control bus)
-	clrf	TRISD, ACCESS	    ; Set PORTD to output
-	setf	PORTD, ACCESS	    ; set control pins to HIGH
+start
 	
-	;setup port E (data bus)
-	banksel	PADCFG1		    ; compiler finds correct bank
-	bsf	PADCFG1, REPU, BANKED	; activate PORTE pull up resistors 
-	movlb	0x00		    ; set BSR back to 0
-	setf	TRISE		    ; Tri-state PORTE
+SPI_MasterInit	; Set Clock edge to positive
+    bsf	    SSP2STAT, CKE
+    ; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+    movlw   (1<<SSPEN)|(1<<CKP)|(0x02)
+    movwf   SSP2CON1
+    ; SDO2 output; SCK2 output
+    bcf	    TRISC, SDO2
+    bcf	    TRISC, SCK2
+    return 
+
+SPI_MasterTransmit  ; Start transmission of data (held in W)
+    movwf 	SSP2BUF 
+Wait_Transmit	; Wait for transmission to complete 
+    btfss 	PIR2, SSP2IF
+    bra 	Wait_Transmit
+    bcf 	PIR2, SSP2IF	; clear interrupt flag
+    return
+
 	
-	;setup ouput ports F-G for visualizing  data
-	movlw	0x00
-	movwf	TRISH, ACCESS	    ; set PORTF to ouput
-	movwf	TRISJ, ACCESS	    ; set PORTG to ouput
-	
-main	;write a byte to reg 1 and read into port H
-	movlw	0xEE
-	movwf	0x20, ACCESS	    ; want to write 0xF9
-	bcf	0x10, 0, ACCESS	    ; set bit to 0, so write to 1st register
-	call	write		    ; write byte
-	call	read		    ; read from same register
-	movff	0x21, PORTH	    ; display contents of register in PORTF
-	
-	;write a byte to reg 2 and read into port J
-	movlw	0xAA
-	movwf	0x20, ACCESS	    ; want to write 0xE5
-	bsf	0x10, 0, ACCESS	    ; set bit to 1, so write to 2nd register
-	call	write		    ; write byte
-	call	read		    ; read from same register
-	movff	0x21, PORTJ	    ; display contents of register in PORTJ
-	GOTO	start		    ; loop back to start
-	
-	
-	; writes data 0x20 to memory
-write   movlw	0x00
-	movwf	TRISE, ACCESS	    ; set PORTE to output
-	movff	0x20, PORTE	    ; write data to bus
-	btfss	0x10, 0, ACCESS	    ; check 0-bit of 0x10, skip if 1
-	movlw	0xFE		    ; set CP1 to 0
-	btfsc	0x10, 0 , ACCESS    ; check 0-bit of 0x10, skip if 0
-	movlw	0xFB		    ; set CP2 to 0
-	movwf	PORTD, ACCESS	    ; write to port D
-	NOP			    ; wait
-	movlw	0xFF
-	movwf	PORTD, ACCESS	    ; reset control bus to high
-	setf	TRISE		    ; Tri-state PORTE
-	return	0
-	
-	
-	; read data from memory into 0x21
-read	movlw	0xFF
-	movwf	TRISE, ACCESS	    ; set PORTE to input
-	btfss	0x10, 0, ACCESS	    ; check 0-bit of 0x10, skip if 1
-	movlw	0xFD		    ; set OE1 to 0
-	btfsc	0x10, 0 , ACCESS    ; check 0-bit of 0x10, skip if 0
-	movlw	0xF7		    ; set OE2 to 0
-	movwf	PORTD, ACCESS	    ; write to port D
-	NOP			    ; wait
-	movff	PORTE, 0x21	    ; save to register 0x21
-	movlw	0xFF
-	movwf	PORTD, ACCESS	    ; reset control bus to HIGH
-	setf	TRISE		    ; Tri-state PORTE
-	return	0
-	
-	end
+    end
